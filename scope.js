b9d5645a411a559995c1d872cab64020cef0059e
przemskydev <przemekgithub@o2.pl>
2022-11-28 22:03:55 +0100

    nested function scope

--- scope.js
+++ scope.js
@@ -5,11 +5,15 @@ Global scope
 Nested function
 */
 
+const globalScope = "This is global scope variable value";
+const scopeVariable = "This is GLOBAL SCOPE variable value named the same as block and function scope variable";
+
 // *** BLock scope ***
 
 if (true) {
   const scopeVariable = "This is block scope variable value";
   console.log(scopeVariable); //This is block scope variable value
+  console.log(globalScope); //This is global scope variable value
 }
 // console.log(blockScopeVariable) //ReferenceError: blockScopeVariable is not defined
 
@@ -17,11 +21,21 @@ if (true) {
 
 const functionScope = () => {
   const scopeVariable = "This is function scope variable value";
-  // console.log(scopeVariable) //This is function scope variable value
+  console.log(scopeVariable) //This is function scope variable value
+  console.log(globalScope) //This is global scope variable value
 };
 functionScope();
 // console.log(functionScopeVariable) //ReferenceError: functionScopeVariable is not defined
 
-// *** GLobal scope ***
+// *** Nested Function scope ***
 
-
+const num1 = 100;
+function outer() {
+    const num2 = 200;
+     function inside() {
+        const num3 = 300;
+        console.log(num1, num2, num3);
+     }
+     inside();
+}
+outer();
