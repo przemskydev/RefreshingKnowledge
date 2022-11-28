2c6a34b77f13847f176510167638023e596a56f4
przemskydev <przemekgithub@o2.pl>
2022-11-28 22:49:16 +0100

    closure descr

--- closure.js
+++ closure.js
@@ -1,16 +1,20 @@
 /*
+Closure is created when function return another function. In line 15 we are returning inside() function from the out() function.
+JavaScript return it as a scope with one variable called couter, together terms as a closure and will remember counter variable value.
+Inside() function has access to the counter variable in the out() function scope even after the out() function has finished
 
 */
 
 function out() {
-    let counter = 0;
+  let counter = 0;
 
-    function inside(params) {
-        counter++;
-        console.log(counter)
-    };
-    inside();
+  function inside() {
+    counter++;
+    console.log(counter);
+  }
+  return inside;
 }
-out();
+let closureFunction = out();
 
-out();
+closureFunction(); // 1
+closureFunction(); // 2
