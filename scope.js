2be2b948379b608430390db2c8b2b5ce03c0f5e5
przemskydev <przemekgithub@o2.pl>
2022-11-28 21:44:48 +0100

    block scope quick example

---
+++ scope.js
@@ -0,0 +1,12 @@
+/*
+BLock scope
+Function scope
+Blobal scope
+Nested function
+*/
+
+if (true) {
+    const blockScopeVariable = "This is block scope variable value";
+}
+
+console.log(blockScopeVariable) //ReferenceError: blockScopeVariable is not defined
